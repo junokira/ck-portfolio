fe2357815e0367cca5447c1b64279415615b605c
junoryuk <junokira777@gmail.com>
2025-08-08 11:31:21 +0200

    Initial commit: CK Portfolio

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// Tailwind entry (already set up below)
+import "./index.css";
+
+// Optional: if you want extra styles in app.css, keep this import
+import "./app.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
